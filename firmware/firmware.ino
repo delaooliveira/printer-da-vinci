3c330c4400c2ea4ce850e304c41a3781dfbe3cd8
delaooliveira <andredelao@gmail.com>
2018-05-15 00:09:26 -0300

    Started implementing linear motion functions

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -9,25 +9,28 @@ void home(Stepper, Stepper);
 
 //Global variables
 String command;
-const int stepsPerRevolution = 2048;
  
 void setup() {
   
-  // Definitions of the pins
-  #define X_end 1
-  #define Y_end 1
-  #define Z_actuator 1
-  #define X_step_1 1
-  #define X_step_2 1
-  #define X_step_3 1
-  #define X_step_4 1
-  #define Y_step_1 1
-  #define Y_step_2 1
-  #define Y_step_3 1
-  #define Y_step_4 1  
-  #define start 1
-  #define emergency 1
-  #define MAX_MOTOR_SPEED 10
+  // Definitions of the pins and constants
+  #define X_end               1
+  #define Y_end               1
+  #define Z_actuator          1
+  #define X_step_1            1
+  #define X_step_2            1
+  #define X_step_3            1
+  #define X_step_4            1
+  #define Y_step_1            1
+  #define Y_step_2            1
+  #define Y_step_3            1
+  #define Y_step_4            1  
+  #define start               1 
+  #define emergency           1
+  #define stepsPerRevolution  2048
+  #define anglePerStep        0.00306796157 // 2*pi/stepsPerRevolution | Unit: rad
+  #define gearRadius          25            // Unit: mm
+  #define gearRatio           10
+  #define MAX_MOTOR_SPEED     10            // Unit: RPM
 
   // Modes of each pin defined above
   pinMode(X_end, INPUT);
@@ -68,17 +71,35 @@ void loop() {
 
 void read_command(String *command){
 
-  *command = Serial.readString();
-  Serial.write("Command Recieved");
+  if (Serial.available() > 0){
+    *command = Serial.readString();
+    Serial.write("Command Recieved");
+  }
   
 }
 
-void home(Stepper Motor_x, Stepper Motor_y){
+void G28(Stepper Motor_x, Stepper Motor_y){
   while(digitalRead(X_end) == 0){
     Motor_x.step(-stepsPerRevolution);
   }
   while(digitalRead(Y_end) == 0){
     Motor_y.step(-stepsPerRevolution);
   }
+}
+
+void G00(Stepper Motor_x, Stepper Motor_y, float x_desired, float y_desired){
+
+  //Calculating the number os steps each axis should move
+  int xSteps, ySteps, meanStep;
+  xSteps = round(x_desired*anglePerStep*gearRadius*gearRatio);
+  ySteps = round(y_desired*anglePerStep*gearRadius*gearRatio);
+
+  meanStep = (xSteps+ySteps)/2;
+  
+  for (int i=0; i<meanStep; i++){
+    Motor_x.step(meanStep);   // FIX-ME: x and y steps are differents // Should do weighted average or sometinhg like that so they reach end point together
+    Motor_y.step(meanStep);
+  }
+  
 }
 
